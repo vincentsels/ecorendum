b6a7068aed73d9d506397d148a7607d66a1c6cae
Vincent Sels <vincent_sels@hotmail.com>
2021-11-26 22:49:02 +0100

    Add project filter

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 import { ProposalService } from './proposal.service';
 
@@ -8,9 +9,13 @@ import { ProposalService } from './proposal.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  constructor(public proposalService: ProposalService) {}
+  constructor(public proposalService: ProposalService) {
+    this.filteredProjects = proposalService.proposals;
+  }
 
+  filteredProjects: Proposal[] = [];
   resultsVisible = false;
+  projectsFilter = '';
 
   setResultsInViewport({ visible }: { visible: boolean }) {
     this.resultsVisible = visible;
@@ -18,5 +23,21 @@ export class MainComponent {
 
   navigateToResults(el: HTMLElement) {
     el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  filterChanged() {
+    if (this.projectsFilter) {
+      this.filteredProjects = this.proposalService.proposals.filter(
+        p => p.title.some(t => t.text.includes(this.projectsFilter)) ||
+          p.summary.some(t => t.text.includes(this.projectsFilter))
+      );
+    } else {
+      this.filteredProjects = this.proposalService.proposals;
+    }
+  }
+
+  clearFilter() {
+    this.projectsFilter = '';
+    this.filterChanged();
   }
 }
