187d8ee7e1624edb6ce1986dbe39a26daa4dd8e2
Sels Vincent <PXH816@fluvius.be>
2023-12-22 22:01:40 +0100

    Some missing translations and improved common dialog

--- src/app/common/dialog.component.ts
+++ src/app/common/dialog.component.ts
@@ -4,13 +4,10 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dial
 @Component({
   selector: 'common-dialog',
   template: `
-  <button mat-icon-button class="close-button" [mat-dialog-close]="true">
-    <mat-icon>close</mat-icon>
-  </button>
   <h1 mat-dialog-title *ngIf="data.title">
     {{ data.title }}
   </h1>
-  <div mat-dialog-content style="min-width: 250px">
+  <mat-dialog-content style="min-width: 250px">
     <div [class]="data.contentClass" *ngIf="data.content">{{ data.content }}</div>
     <markdown *ngIf="data.contentMdData" [data]="data.contentMdData">{{ data.content }}</markdown>
     <markdown *ngIf="data.contentMdSrc" [src]="data.contentMdSrc" (load)="onLoad()" (error)="onError($event)"></markdown>
@@ -20,12 +17,11 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dial
     <div *ngIf="data.contentMdSrc && !contentMdSrcLoaded && contentMdSrcError">
       {{ contentMdSrcError }}
     </div>
-  </div>
-  <div mat-dialog-actions>
+  </mat-dialog-content>
+  <mat-dialog-actions>
     <button mat-button mat-dialog-close>{{ data.buttonText || ('Ok' | translate) }}</button>
-  </div>`,
+  </mat-dialog-actions>`,
   styles: [
-    `.close-button { float: right; }`,
   ]
 })
 export class CommonDialogComponent {
